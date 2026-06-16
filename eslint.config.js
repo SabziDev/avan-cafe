8b2853120c059987c64a7c4160b31193f29f349f
Abolfazl Sabzmohammadi <SabziDev@gmail.com>
2026-06-16 19:40:35 +0330

    optimize

--- eslint.config.js
+++ eslint.config.js
@@ -2,16 +2,25 @@ import { defineConfig } from "@fullstacksjs/eslint-config";
 import reactRefresh from "eslint-plugin-react-refresh";
 import unicorn from "eslint-plugin-unicorn";
 
-import addBlankLineBeforeReturn from "./eslint-configs/padding/add-blank-line-before-return/add-blank-line-before-return.js";
 import mergeDuplicateIdAndClassNameProps from "./eslint-configs/merge/merge-duplicate-id-and-classname-props/merge-duplicate-id-and-classname-props.js";
 import mergeExports from "./eslint-configs/merge/merge-exports/merge-exports.js";
 import noInvalidIdAndClassNameValue from "./eslint-configs/no-invalid/no-invalid-id-and-classname-value/no-invalid-id-and-classname-value.js";
 import noUselessTemplateLiteral from "./eslint-configs/no-useless/no-useless-template-literal/no-useless-template-literal.js";
+import addBlankLineBeforeReturn from "./eslint-configs/padding/add-blank-line-before-return/add-blank-line-before-return.js";
 import sortComments from "./eslint-configs/sort/sort-comments/sort-comments.js";
 import sortJsxProps from "./eslint-configs/sort/sort-jsx-props/sort-jsx-props.js";
 import sortObjectProps from "./eslint-configs/sort/sort-object-props/sort-object-props.js";
 
-const customPlugin = {
+const externalPluginsRules = {
+  "unicorn/consistent-function-scoping": "warn",
+
+  "unicorn/filename-case": "off",
+  "unicorn/prefer-global-this": "off",
+  "unicorn/prevent-abbreviations": "off",
+  "unicorn/no-array-sort": "off",
+  "unicorn/no-null": "off",
+};
+const customRules = {
   rules: {
     "add-blank-line-before-return":
       addBlankLineBeforeReturn.rules["add-blank-line-before-return"],
@@ -36,51 +45,44 @@ const customPlugin = {
     "sort-object-props": sortObjectProps.rules["sort-object-props"],
   },
 };
+const baseRules = {
+  "func-style": ["warn", "expression"],
+  quotes: [
+    "error",
+    "double",
+    { avoidEscape: true, allowTemplateLiterals: false },
+  ],
+  eqeqeq: ["error", "always"],
+  "no-console": "warn",
+
+  "jsx-a11y/click-events-have-key-events": "off",
+  "jsx-a11y/no-noninteractive-element-interactions": "off",
+
+  "custom/add-blank-line-before-return": "warn",
+  "custom/merge-duplicate-id-and-classname-props": "warn",
+  "custom/merge-exports": "warn",
+  "custom/no-invalid-id-and-classname-value": "error",
+  "custom/no-useless-template-literal": "warn",
+  "custom/sort-comments": "warn",
+  "custom/sort-jsx-props": "warn",
+  "custom/sort-object-props": "warn",
+};
 
 const config = defineConfig(
   {
     tailwind: { entryPoint: "./src/input.css" },
 
     plugins: {
-      custom: customPlugin,
+      custom: customRules,
     },
 
-    rules: {
-      "func-style": ["warn", "expression"],
-      quotes: [
-        "error",
-        "double",
-        { avoidEscape: true, allowTemplateLiterals: false },
-      ],
-      eqeqeq: ["error", "always"],
-      "no-console": "warn",
-
-      "jsx-a11y/click-events-have-key-events": "off",
-      "jsx-a11y/no-noninteractive-element-interactions": "off",
-
-      "custom/add-blank-line-before-return": "warn",
-      "custom/merge-duplicate-id-and-classname-props": "warn",
-      "custom/merge-exports": "warn",
-      "custom/no-invalid-id-and-classname-value": "error",
-      "custom/no-useless-template-literal": "warn",
-      "custom/sort-comments": "warn",
-      "custom/sort-jsx-props": "warn",
-      "custom/sort-object-props": "warn",
-    },
+    rules: baseRules,
   },
 
   reactRefresh.configs.vite,
   unicorn.configs.recommended,
   {
-    rules: {
-      "unicorn/consistent-function-scoping": "warn",
-
-      "unicorn/filename-case": "off",
-      "unicorn/prefer-global-this": "off",
-      "unicorn/prevent-abbreviations": "off",
-      "unicorn/no-array-sort": "off",
-      "unicorn/no-null": "off",
-    },
+    rules: externalPluginsRules,
   },
 );
 
