3489aa716ba4bf61d433c886997680f83a0e2402
Abolfazl Sabzmohammadi <SabziDev@gmail.com>
2026-06-26 10:55:36 +0330

    optimize

--- src/pages/Home/sections/ReserveYourTableSection/components/ReserveYourTableForm/Form/Textarea.jsx
+++ src/pages/Home/sections/ReserveYourTableSection/components/ReserveYourTableForm/Form/Textarea.jsx
@@ -2,7 +2,7 @@ const Textarea = ({ inputField }) => {
   return (
     <textarea
       placeholder={inputField.placeholder}
-      className="h-40 w-full rounded-xl bg-cream px-4 py-1 placeholder:text-sm placeholder:text-primary/40 sm:col-span-2"
+      className="h-40 w-full resize-none rounded-xl bg-cream px-4 py-1 placeholder:text-sm placeholder:text-primary/40 sm:col-span-2"
     />
   );
 };
