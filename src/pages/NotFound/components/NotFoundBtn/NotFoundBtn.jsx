ff51ca264c4a162db9c4cf1293b792f8239463b0
Abolfazl Sabzmohammadi <SabziDev@gmail.com>
2026-08-21 18:08:55 +0330

    update: NotFound-page

--- src/pages/NotFound/components/NotFoundBtn/NotFoundBtn.jsx
+++ src/pages/NotFound/components/NotFoundBtn/NotFoundBtn.jsx
@@ -2,13 +2,11 @@ import { Link } from "react-router";
 
 const NotFoundBtn = () => {
   return (
-    // TODO NotFound-Page Btn-Text
     <Link
       to="/"
       className="z-10 rounded-lg bg-white/90 p-3 font-medium text-black/60 shadow-xl hover:shadow-2xl active:scale-[0.96]"
     >
       بازگشت به صفحه اصلی
-      {/* Return Home */}
     </Link>
   );
 };
